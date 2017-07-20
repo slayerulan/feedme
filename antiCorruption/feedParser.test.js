d2270dafb9cfa5836983cfc3fef4d5e7d53ae884
Macs Dickinson <m.dickinson@ticketarena.co.uk>
2017-07-20 18:10:18 +0100

    removed as for testing purposes only

--- antiCorruption/feedParser.test.js
+++ antiCorruption/feedParser.test.js
@@ -1,19 +1,6 @@
 import test from 'ava';
 import parser from './feedParser';
 
-test('create packet success', t => {
-    // arrange
-    const input = '|2049|create|packet|1500560941381|XYZ|';
-    // act
-    const packet = parser.parse(input);
-    // assert
-    t.is(packet.msgId, '2049', 'packet.msgId invalid');
-    t.is(packet.operation, 'create', 'packet.operation invalid');
-    t.is(packet.type, 'packet', 'packet.type invalid');
-    t.is(packet.timestamp.getTime(), new Date(1500560941381).getTime(), 'packet.timestamp invalid');
-    t.truthy(packet.body, 'packet.body invalid');
-});
-
 test('create event success', t => {
     // arrange
     const input = '|2049|create|event|1500560941381|f5e8fcd3-8f20-40b3-826e-f97bf95f1423|Football|Premier League|\\|Manchester Utd\\| vs \\|Manchester City\\||1500560978604|0|1|';
